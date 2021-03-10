50cc66e195ce3faae019070285290a94c5cde182
Nike <reintamm.nike@gmail.com>
2021-03-10 12:24:01 +0900

    shortening code
    shortened js code where possible as there was too many dublicate functions that made code hard to read. rearranged things in html

--- src/script.js
+++ src/script.js
@@ -50,38 +50,27 @@
             let minute = now.getMinutes();
                 if (minute < 10) 
                 {minute= `0${minute}`};
-            let date2Sentnce = `Current Time ${hour}:${minute}`;
+            let date2Sentnce = `${hour}:${minute}`;
             return date2Sentnce;
         }
 //output of date and time
 dateoutput.innerHTML = formatDate();
 time.innerHTML = timeFunction();
 
+    
 
-//default display weather is tokyo
-    function formatPlaceDefault(event) 
+//output for seaerched city/button response
+function dataWeather(response)
     {
-        let apiKey = `ea83478d04ea758c32daab15e512ea29`;
-        let degreeSystem = `metric`;
-        let cityName=`Tokyo`
-        let url = `https://api.openweathermap.org/data/2.5/weather?`;
-        let urlWeatherDefault = `${url}q=${cityName}&appid=${apiKey}&units=${degreeSystem}`;
-        axios.get(urlWeatherDefault).then(dataWeatherDefault);
-    }
-
-    function dataWeatherDefault(response)
-    {
-        console.log(response.data);
-
         //for city
         let htmlCityValue = response.data.name;
         let htmlCity = document.querySelector("#city");
         htmlCity.innerHTML = htmlCityValue;
 
-        //degree
+        //degree in celsius
         let tempRound = Math.round(response.data.main.temp);
         let tempChange = document.querySelector("#degree-today");
-        tempChange.innerHTML = tempRound;
+        tempChange.innerHTML = (`${tempRound}℃`);
 
         //humidity
         let humRound = Math.round(response.data.main.humidity);
@@ -97,73 +86,37 @@ time.innerHTML = timeFunction();
         let skyChange = document.querySelector("#sky");
         skyChange.innerHTML = `${response.data.weather[0].description}`;
 
-
         //icon
         let weatherIcon= document.querySelector("#large-image")
         weatherIcon.setAttribute("src" ,`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    
+
         //broken icon
         let weatherBroken= document.querySelector("#large-image")
         weatherBroken.setAttribute("alt" ,`${response.data.weather[0].description}`);
     }
+    
 
-formatPlaceDefault();
-
+//default api attributes
+    let apiKey = `ea83478d04ea758c32daab15e512ea29`;
+    let degreeSystem = `metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?`;
+    
 
 //location api for searched city
 
-    function formatPlace(event) 
+    function formatPlace(city) 
     {
-        event.preventDefault();
-        let result = document.querySelector("#search-text");
-        let cityName = result.value;
-        let apiKey = `ea83478d04ea758c32daab15e512ea29`;
-        let degreeSystem = `metric`;
-        let url = `https://api.openweathermap.org/data/2.5/weather?`;
-        let urlWeather = `${url}q=${cityName}&appid=${apiKey}&units=${degreeSystem}`;
+        let urlWeather = `${url}q=${city}&appid=${apiKey}&units=${degreeSystem}`;
         axios.get(urlWeather).then(dataWeather);
+     }
+
+    function searchValue(event)
+    {  event.preventDefault();      
+        let result = document.querySelector("#search-text");
+        formatPlace(result.value);
     }
 
-//output for seaerched city
-    function dataWeather(response)
-    {
-        //for city
-        let htmlCityValue = response.data.name;
-        let htmlCity = document.querySelector("#city");
-        htmlCity.innerHTML = htmlCityValue;
-
-        //degree
-        let tempRound = Math.round(response.data.main.temp);
-        let tempChange = document.querySelector("#degree-today");
-        tempChange.innerHTML = tempRound;
-
-        //humidity
-        let humRound = Math.round(response.data.main.humidity);
-        let humChange = document.querySelector("#humidity");
-        humChange.innerHTML = `Humidity ${humRound} %`;
-
-        //windspeed
-        let windRound = response.data.wind.speed;
-        let windChange = document.querySelector("#wind-speed");
-        windChange.innerHTML = `Wind speed ${windRound} m/s`;
-
-        //sky
-        let skyChange = document.querySelector("#sky");
-        skyChange.innerHTML = `${response.data.weather[0].description}`;
-
-        //icon
-        let weatherIcon= document.querySelector("#large-image")
-        weatherIcon.setAttribute("src" ,`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-
-        //broken icon
-        let weatherBroken= document.querySelector("#large-image")
-        weatherBroken.setAttribute("alt" ,`${response.data.weather[0].description}`);
-    }
-
-    let searchForm = document.querySelector("#searchEngine,#search-button");
-    searchForm.addEventListener("submit",formatPlace);
-
-//api for current location
+ //api for current location
 
     function getLocation() 
     {
@@ -174,53 +127,16 @@ formatPlaceDefault();
     {
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
-        let apiKey = `ea83478d04ea758c32daab15e512ea29`;
-        let degreeSystem = `metric`;
-        let url = `https://api.openweathermap.org/data/2.5/weather?`;
         let urlWeather22 = `${url}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${degreeSystem}`;
-        axios.get(urlWeather22).then(currentLocation);
+        axios.get(urlWeather22).then(dataWeather);
     }
-
+    
+    let searchForm = document.querySelector("#searchEngine,#search-button");
+    searchForm.addEventListener("submit",searchValue);
 
 //output for current location
-
-    function currentLocation(response) 
-    {
-        //city
-        let htmlCityValue = response.data.name;
-        let htmlCity = document.querySelector("#city");
-        htmlCity.innerHTML = htmlCityValue;
-
-        //degree-today
-        let tempRound = Math.round(response.data.main.temp);
-        let tempChange = document.querySelector("#degree-today");
-        tempChange.innerHTML = tempRound;
-
-        //humidity
-        let humRound = Math.round(response.data.main.humidity);
-        let humChange = document.querySelector("#humidity");
-        humChange.innerHTML = `Humidity ${humRound} %`;
-
-        //wind-speed
-        let windRound = response.data.wind.speed;
-        let windChange = document.querySelector("#wind-speed");
-        windChange.innerHTML = `Wind speed ${windRound} m/s`;
-
-        //sky
-        let skyChange = document.querySelector("#sky");
-        skyChange.innerHTML = `${response.data.weather[0].description}`;
-
-        //icon
-        let weatherIcon= document.querySelector("#large-image")
-        weatherIcon.setAttribute("src" ,`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-
-        //broken icon
-        let weatherBroken= document.querySelector("#large-image")
-        weatherBroken.setAttribute("alt" ,`${response.data.weather[0].description}`);
-    }
 
     let locationForm = document.querySelector("#search-location");
     locationForm.addEventListener("click",getLocation);
 
-
-    
+    formatPlace("tokyo") ;
